f0b79d3f4d917636c00ff7d6f035441b1e7b540a
kondatr <a.kondratsky@mail.ru>
2024-09-12 18:45:44 +0300

    final

---
+++ main.py
@@ -0,0 +1,8 @@
+from graphs.EBITDA import EBITDA_graph, av_med_EBITDA_graph
+import matplotlib.pyplot as plt
+from graphs.revenue import revenue_map, revenue_pie_graph
+from graphs.net_profit import growth_years_graph, net_profit_graph, net_profit_change_graph
+
+
+EBITDA_graph()
+plt.show()
